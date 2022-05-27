543ad5418b3e3b795a72227269aa6fb5caac6ee2
Todd Kushner <kushnertodd@gmail.com>
2022-05-27 09:17:49 -0600

    added excluded chars

--- wordle.py
+++ wordle.py
@@ -2,7 +2,7 @@
 wordle.py - produce possible wordle words after having guessed some words
 
 usage:
-    python wordle.py possible required exact
+    python wordle.py possible required exact excluded
 where:
     possible  all the letters that could be in the word
               all the letters that aren't dark grey on the keyboard
@@ -14,13 +14,17 @@ where:
                    "." if the letter is yellow to grey at that position
                    so t-green r-green i-yellow a-yellow l-grey
                        is coded as "tr..."
+    excluded  the letters known not to be at a position, in the form
+                  lll-lll-lll--lll
 requirements:
     a dictionary file must be in "dict5.txt"
 '''
 
 import sys
 import re
-def check_dictionary_word(dictionary_word, possible, required, exact):
+def check_dictionary_word(dictionary_word, possible, required, exact_chars, excluded_chars):
+    debug = True
+    #print(f'check_dictionary_word({dictionary_word}, {possible}, {required}, {exact_chars}, {excluded_chars}')
     # check that all possible characters are in the dictionary word
     for c in dictionary_word:
         if not c in possible:
@@ -29,24 +33,38 @@ def check_dictionary_word(dictionary_word, possible, required, exact):
     for c in required:
         if not c in dictionary_word:
             return False
+    # check that excluded characters are not matched
+    for i in range(5):
+        x = excluded_chars[i]
+        c = dictionary_word[i]
+        if c in x:
+            #print(f'{dictionary_word} failing "{c}" at "{i}" in "{x}" of "{excluded_chars}"')
+            return False
     # check that the dictionary word matches the letters in the exact position
-    if re.search(exact, dictionary_word):
+    if re.search(exact_chars, dictionary_word):
         return True
     else:
         return False
 
 # main program
 if len(sys.argv) < 4:
-    print(f'usage: python wordle.py possible must exact')
+    print(f'usage: python wordle.py possible must exact excluded"')
+    print(f'    e.g, "python wordle.py qweriosdflcvn eosd ..s.. e--od-s-"')
     exit(0)
 dict = "dict5.txt"
 possible = sys.argv[1]
 required = sys.argv[2]
-exact = sys.argv[3]
+exact_chars = sys.argv[3]
+if len(exact_chars) != 5:
+    print(f'exact characters must have 5 letters: "{exact_chars}"')
+excluded = sys.argv[4]
+excluded_chars = excluded.split("-")
+if len(excluded_chars) != 5:
+    print(f'excluded characters must have 5 "-"-delimitedfields: "{excluded_chars}"')
 f = open(dict)
 for next_line in f:
     dictionary_word = next_line.strip()  # remove newline
-    if check_dictionary_word(dictionary_word, possible, required, exact):
+    if check_dictionary_word(dictionary_word, possible, required, exact_chars, excluded_chars):
         print(dictionary_word)
 f.close()
 
